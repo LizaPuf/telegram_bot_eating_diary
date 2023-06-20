7c2514b907f6237af428aa1ac299132e0e37ac25
Elizaveta Slonova <pufliza@gmail.com>
2023-06-20 13:09:45 +0300

    refactoring add_eating function 2

--- commands.py
+++ commands.py
@@ -1,13 +1,15 @@
-eating_diary = {}
+from collections import defaultdict
+
+eating_diary = defaultdict(dict)
 
 
 def add_eating(message, eating_diary):
     split_message = message.text.split(maxsplit=6)
     _, date, meal, *rest = split_message
-    if date in eating_diary:
-        eating_diary[date][meal] = rest
-    else:
-        eating_diary[date] = {meal:rest}
+    #if date in eating_diary:
+    eating_diary[date][meal] = rest
+    #else:
+    #    eating_diary[date] = {meal:rest}
 
 
 def del_eating(message, eating_diary):

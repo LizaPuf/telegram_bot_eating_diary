bbaed1ed3ec9599605879ac842baada0859f10ad
Elizaveta Slonova <pufliza@gmail.com>
2023-06-20 13:00:50 +0300

    refactoring add_eating function

--- commands.py
+++ commands.py
@@ -3,10 +3,15 @@ eating_diary = {}
 
 def add_eating(message, eating_diary):
     split_message = message.text.split(maxsplit=6)
-    if split_message[1] in eating_diary:
-        eating_diary[split_message[1]][split_message[2]] = split_message[3:]
+    _, date, meal, *rest = split_message
+    if date in eating_diary:
+        eating_diary[date][meal] = rest
     else:
-        eating_diary[split_message[1]] = {split_message[2]:split_message[3:]}
+        eating_diary[date] = {meal:rest}
+
+
+def del_eating(message, eating_diary):
+    split_message = message.text.split()
 
 
 

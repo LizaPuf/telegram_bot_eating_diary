70e42e8ffe5e3a3e742f2c5344f1077316644b34
Elizaveta Slonova <pufliza@gmail.com>
2023-06-21 13:08:03 +0300

    add rest function for bot

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 eating_diary = defaultdict(dict)
+emotions_diary = defaultdict(dict)
 
 class BedDeleteCommand(Exception):
     pass
@@ -9,10 +10,29 @@ class BedDeleteCommand(Exception):
 def add_eating(message, eating_diary):
     split_message = message.text.split(maxsplit=5)
     _, date, meal, *rest = split_message
-    #if date in eating_diary:
     eating_diary[date][meal] = rest
-    #else:
-    #    eating_diary[date] = {meal:rest}
+
+def add_emotions(message, emotions_diary):
+    _, date, meal, *rest = message.text.split(maxsplit=3)
+    emotions_diary[date][meal] = rest
+
+def show_eat(message, eating_diary):
+    split_message = message.split()
+    result = []
+    for meal, info in eating_diary[split_message[1]].items():
+        result.append(f'{meal} - {info[-1]}')
+        result.append(f'чувство голода до приема пищи - {info[0]}')
+        result.append(f'чувство голода после приема пищи - {info[1]}')
+        result.append('')
+    return '\n'.join(result)
+
+def show_emotions(message, emotions_diary):
+    split_message = message.split()
+    result = []
+    for meal, info in emotions_diary[split_message[1]].items():
+        result.append(f'{meal} - {info[-1]}')
+    return '\n'.join(result)
+
 
 
 def del_eating(message, eating_diary):
@@ -24,16 +44,15 @@ def del_eating(message, eating_diary):
     else:
         raise BedDeleteCommand
 
+def del_emotions(message, emotions_diary):
+    split_message = message.text.split()
+    if len(split_message) == 2:
+        del emotions_diary[split_message[1]]
+    elif len(split_message) > 2:
+        del emotions_diary[split_message[1]][split_message[2]]
+    else:
+        raise BedDeleteCommand
 
-def show_eat(message, eating_diary):
-    split_message = message.split()
-    result = []
-    for meal, info in eating_diary[split_message[1]].items():
-        result.append(f'{meal} - {info[-1]}')
-        result.append(f'чувство голода до приема пищи - {info[0]}')
-        result.append(f'чувство голода после приема пищи - {info[1]}')
-        result.append('')
-    return '\n'.join(result)
 
 
 '''
--- main.py
+++ main.py
@@ -2,16 +2,6 @@ import telebot
 import commands
 import constants
 
-'''
-1) Поправить функцию удаления. Одновременно выводит команда не найдена и щапись удалена
-2) Создать функции по добавлению эмоций 
-3) Создать функцию по удалению эмоций 
-4) Добавить функцию вывода в тг бот приемов пищи - done
-5) Добавить функцию вывода в тг бот эмоций 
-6) Добавить информационую справку о интуитивном питании 
-7) Добавить информационную справку о возможностях тг бота 
-8) Добавить функцию, которая будет ловить ошибки 
-'''
 
 TOKEN_API = '5958215581:AAGxUmz9lfKBWIsRymKArGI-b0WTvUkUdnw'
 
@@ -28,7 +18,24 @@ def help(message):
     bot.send_message(message.chat.id, 'Команда добавлена')
     print(commands.eating_diary)
 
-@bot.message_handler(commands=['del'])
+
+@bot.message_handler(commands=['add_emotions'])
+def help(message):
+    commands.add_emotions(message, commands.emotions_diary)
+    bot.send_message(message.chat.id, 'Команда добавлена')
+    print(commands.emotions_diary)
+
+@bot.message_handler(commands=['show_eat'])
+def help(message):
+    formated_text = commands.show_eat(message.text, commands.eating_diary)
+    bot.send_message(message.chat.id, formated_text)
+
+@bot.message_handler(commands=['show_emotions'])
+def help(message):
+    formated_text = commands.show_emotions(message.text, commands.emotions_diary)
+    bot.send_message(message.chat.id, formated_text)
+
+@bot.message_handler(commands=['del_eat'])
 def help(message):
     try:
         commands.del_eating(message, commands.eating_diary)
@@ -36,18 +43,23 @@ def help(message):
         bot.send_message(message.chat.id, 'Неправильно введена команда')
     except KeyError:
         bot.send_message(message.chat.id, 'Неправильно введена дата или прием пищи')
+    else:
+        bot.send_message(message.chat.id, 'Запись удалена')
+        print(commands.eating_diary)
 
-    bot.send_message(message.chat.id, 'Запись удалена')
-    print(commands.eating_diary)
-
-@bot.message_handler(commands=['add_emotions'])
+@bot.message_handler(commands=['del_emotions'])
 def help(message):
-   bot.send_message(message.chat.id, 'Работает')
+    try:
+        commands.del_emotions(message, commands.emotions_diary)
+    except commands.BedDeleteCommand:
+        bot.send_message(message.chat.id, 'Неправильно введена команда')
+    except KeyError:
+        bot.send_message(message.chat.id, 'Неправильно введена дата или прием пищи')
+    else:
+        bot.send_message(message.chat.id, 'Запись удалена')
+        print(commands.eating_diary)
 
-@bot.message_handler(commands=['show_eat'])
-def help(message):
-    formated_text = commands.show_eat(message.text, commands.eating_diary)
-    bot.send_message(message.chat.id, formated_text)
+
 
 @bot.message_handler(commands=['INFO'])
 def help(message):
